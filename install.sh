6f111bb883444f61558f28205e6156e204d70f03
Misha Manulis <misha@manulis.com>
2010-11-26 13:13:06 -0800

    Forgot to update link creation for vim

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ elif [[ -e $HOME/.vimrc.local ]]; then
   echo "vimrc.local exists, moving to $HOME/.vimrc.local.orig"
   mv $HOME/.vimrc.local $HOME/.vimrc.local.orig
 fi
-ln -s $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
+ln -s $HOME/.dotfiles/vim/vimrc.local $HOME/.vimrc.local
 # -----------------------------------------------------------------------------
 # Setup Ruby and friends
 #

cf301400035a9e4fd01b9610107dd645eed1356c
Misha Manulis <misha@manulis.com>
2011-04-05 16:45:31 -0700

    Updating janus

--- ruby/install.sh
+++ ruby/install.sh
@@ -31,4 +31,14 @@ elif [[ -e $HOME/.irbrc ]]; then
   mv $HOME/.irbrc $HOME/.irbrc.orig
 fi
 ln -s $DOTFILES/ruby/irbrc $HOME/.irbrc
+# -----------------------------------------------------------------------------
+# Setup irbrc
+#
+if [[ -h $HOME/.janus.rake ]]; then
+  rm $HOME/.janus.rake
+elif [[ -e $HOME/.janus.rake ]]; then
+  echo ".janus.rake exists, moving to $HOME/.janus.rake.orig"
+  mv $HOME/.janus.rake $HOME/.janus.rake.orig
+fi
+ln -s $DOTFILES/ruby/janus.rake $HOME/.janus.rake
 
---
+++ ruby/janus.rake
@@ -0,0 +1,2 @@
+vim_plugin_task "rvm.vim",          "https://github.com/csexton/rvm.vim.git"
+

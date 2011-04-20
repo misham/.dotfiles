2d6bda39ce3abd0d73fb1a806fb41f51db2a77e8
Misha Manulis <misha@manulis.com>
2011-04-20 13:57:54 -0700

    Updated npm config, janus and not starting NerdTree automatically

--- js/install.sh
+++ js/install.sh
@@ -4,4 +4,14 @@
 # Install nvm
 #
 git clone https://github.com/creationix/nvm.git $HOME/.nvm
+# -----------------------------------------------------------------------------
+# Install npmrc
+#
+if [[ -h $HOME/.npmrc ]]; then
+  rm $HOME/.npmrc
+elif [[ -e $HOME/.npmrc ]]; then
+  echo "npmrc exists, moving to $HOME/.npmrc.orig"
+  mv $HOME/.npmrc $HOME/.npmrc.orig
+fi
+ln -s $DOTFILES/js/npmrc $HOME/.npmrc
 
